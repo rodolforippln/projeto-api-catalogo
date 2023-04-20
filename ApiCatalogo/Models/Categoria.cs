b9b10e4c16624b280f8ade02a5d55c7f7c633d69
Rodolfo <rodolforippln@gmail.com>
2023-04-20 02:25:20 -0300

    Agregando DataAnnotations as entidades para sobrescrever as convenções do entity framework

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,9 +1,18 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Models;
 
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(50)]    
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
--- ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/Models/Produto.cs
@@ -1,12 +1,28 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models;
 
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string? Nome { get; set; }
+
+    [StringLength(250)]
     public string? Descricao { get; set; }
+
+    [Required]
+    [Column(TypeName = "Decimal(10, 2)")]
     public decimal Preco { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string? ImagemUrl { get; set; }
+
     public int Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
